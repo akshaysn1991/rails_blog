89d909f8b11cf55d2bd0c642c51ca52613a17263
akshaysn <akshaysn1991@gmail.com>
2016-07-23 15:53:55 +0000

    button changes and form css

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'posts#index'
+  root 'posts#index'
   get '/about' => 'pages#about'
 
   # Example of regular route:
