07d1abc9b45a18eb603eff9f6ac153bbbdcd77b0
akshaysn <akshaysn1991@gmail.com>
2016-07-23 10:24:58 +0000

    seed user added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ User.create! :email => 'akshaysn1991.com', :password => 'akshay123', :password_confirmation => 'akshay123'
+
